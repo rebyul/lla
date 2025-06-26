df415a2112630ac19a40e087b0dc20b093d6b2f6
rebyul <rebyul@gmail.com>
2025-06-27 02:33:38 +1200

    Create or open db file

--- src/file.c
+++ src/file.c
@@ -1,20 +1,47 @@
 #include <stdio.h>
 
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include "file.h"
 #include "common.h"
-
+#include "file.h"
 
 int create_db_file(char *filename) {
+  int fd = open(filename, O_RDONLY);
+  // File does exist
+  if (fd != -1) {
+    close(fd);
+    printf("File \"%s\" already exists\n", filename);
+    return STATUS_ERROR;
+  }
 
+  fd = open(filename, O_RDWR | O_CREAT, 0644);
+
+  if (fd == -1) {
+    perror("open");
+    return STATUS_ERROR;
+  }
+
+  return fd;
 }
 
 int open_db_file(char *filename) {
+  int fd = open(filename, O_RDONLY);
+  // File does exist
+  if (fd == -1) {
+    close(fd);
+    printf("File \"%s\" does not exist\n", filename);
+    return STATUS_ERROR;
+  }
 
+  fd = open(filename, O_RDWR, 0644);
+
+  if (fd == -1) {
+    perror("open");
+    return STATUS_ERROR;
+  }
+
+  return fd;
 }
-
-
--- src/main.c
+++ src/main.c
@@ -7,12 +7,18 @@
 #include "file.h"
 #include "parse.h"
 
-void print_usage(char *argv[]) {}
+void print_usage(char *argv[]) {
+  printf("Usage: %s -n -f <database files>\n", argv[0]);
+  printf("\t -n - create new database file\n");
+  printf("\t -f - (required) path to database file\n");
+}
 
 int main(int argc, char *argv[]) {
   int c;
   char *filepath = NULL;
   bool newfile = false;
+
+  int dbfd = -1;
 
   while ((c = getopt(argc, argv, "nf:")) != -1) {
     switch (c) {
@@ -31,6 +37,29 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  if (filepath == NULL) {
+    printf("Filepath is a required argument\n");
+    print_usage(argv);
+
+    return 0;
+  }
+
+  if (newfile) {
+    dbfd = create_db_file(filepath);
+
+    if (dbfd == STATUS_ERROR) {
+      printf("Unable to create database file \"%s\"\n", filepath);
+      return -1;
+    }
+  } else {
+    dbfd = open_db_file(filepath);
+    if (dbfd == STATUS_ERROR) {
+      printf("Unable to open database file \"%s\"\n", filepath);
+    }
+  }
+
   printf("Newfile: %d\n", newfile);
   printf("Filepath: %s\n", filepath);
+
+  return 0;
 }
